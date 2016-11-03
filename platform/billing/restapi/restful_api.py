7860f95997c99f8868f1c3180b8f7b6f4766dc3a
huay001 <yunshu360@163.com>
2016-11-03 17:13:48 +0800

    billing

---
+++ platform/billing/restapi/restful_api.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Author: YanHua <it-yanh@all-reach.com>
+
+import billing_api
+
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+
+
+def rest_app_run():
+
+    app = Flask(__name__)
+    CORS(app=app)
+    api = Api(app)
+
+    api.add_resource(billing_api.ResourcesApi,
+                     '/api/v1.0/billing/resources')
+
+    api.add_resource(billing_api.ResourceApi,
+                     '/api/v1.0/billing/resources/<resource_uuid>')
+
+    api.add_resource(billing_api.VouchersApi,
+                     '/api/v1.0/billing/vouchers')
+
+    api.add_resource(billing_api.VoucherApi,
+                     '/api/v1.0/billing/vouchers/<voucher_uuid>')
+
+    api.add_resource(billing_api.BillsAPI,
+                     '/api/v1.0/billing/bills')
+
+    api.add_resource(billing_api.BalancesApi,
+                     '/api/v1.0/billing/balances')
+
+    api.add_resource(billing_api.OrdersApi,
+                     '/api/v1.0/billing/orders')
+
+    api.add_resource(billing_api.OrderApi,
+                     '/api/v1.0/billing/orders/<order_uuid>')
+
+    app.run(host='0.0.0.0', port=8001, threaded=True, debug=False)
---
+++ platform/billing/rpcapi/rpc_api.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# Author: YanHua <it-yanh@all-reach.com>
+
+import json
+
+from common.logs import logging as log
+from common.acl_auth import acl_check
+from common.code import request_result
+from common.rabbitmq_client import RabbitmqClient
+
+
+class BillingRpcApi(object):
+
+    def __init__(self):
+
+        self.rbtmq = RabbitmqClient()
+        self.queue = 'billing'
+        self.timeout = 60
+
+    @acl_check
+    def resource_create(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_rss_rss_crt", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def resource_delete(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_rss_rss_del", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def resource_update(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_rss_rss_put", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def resource_get(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_rss_rss_get", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def voucher_create(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_voc_voc_crt", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def voucher_update(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_voc_voc_act", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def voucher_get(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_voc_voc_get", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def bill_get(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_bls_bls_get", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def balance_init(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_blc_blc_add", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def balance_update(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_blc_blc_put", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def balance_get(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_blc_blc_get", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def order_create(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_odr_odr_crt", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def order_update(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_odr_odr_put", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
+
+    @acl_check
+    def order_get(self, context, parameters=None):
+        try:
+            dict_data = {"api": "bil_odr_odr_get", "context": context, "parameters": parameters}
+            json_data = json.dumps(dict_data)
+            return json.loads(self.rbtmq.rpc_call_client(self.queue, self.timeout, json_data))
+        except Exception, e:
+            return request_result(598)
