19fa0e030b1cedddf272b7edbe9f6d744d082f77
wang-xf <359876749@qq.com>
2017-02-28 10:48:22 +0800

    …

--- platform/newservice/k8sapi/conf/conf.py
+++ platform/newservice/k8sapi/conf/conf.py
@@ -6,13 +6,20 @@ service_name = 'Ucenter'
 log_level = 'INFO'
 log_file = '/var/log/cloud.log'
 
-mq_server01 = 'boxlinker.com'
-mq_server02 = 'boxlinker.com'
-mq_port = 30001
+# mq_server01 = 'boxlinker.com'
+# mq_server02 = 'boxlinker.com'
+# mq_port = 30001
 
-db_server01 = 'boxlinker.com'
-db_server02 = 'boxlinker.com'
-db_port = 30000
+mq_server01 = 'rabbitmq'
+mq_server02 = 'rabbitmq'
+mq_port = 5672
+
+# db_server01 = 'boxlinker.com'
+# db_server02 = 'boxlinker.com'
+# db_port = 30000
+db_server01 = 'database'
+db_server02 = 'database'
+db_port = 3306
 db_user = 'cloudsvc'
 db_passwd = 'cloudsvc'
 database = 'servicedata'
@@ -29,7 +36,7 @@ rpc_timeout = 60
 STORAGE_HOST = 'https://storage.boxlinker.com/api/v1.0/storage/volumes'
 storage_call_queue = 'storage_call_api'
 TEAM_MSG = 'https://ucenter.boxlinker.com/api/v1.0/ucenter/teams'
-VOLUMEIP = '192.168.1.5:5000,192.168.1.8:5000,192.168.1.9:5000'
-
+#VOLUMEIP = '192.168.1.5:5000,192.168.1.8:5000,192.168.1.9:5000'
+VOLUMEIP = '10.10.10.11:5000,10.10.10.12:5000,10.10.10.21:5000'
 IMAGE_SERVER = 'https://auth.boxlinker.com/api/v1.0/otherserver/images'
 
