c7767e9fcaf95c4c27aabe59aad97a395152e53b
wang-xf <359876749@qq.com>
2017-02-28 11:16:37 +0800

    …

--- platform/newservice/k8sapi/common/logs.py
+++ platform/newservice/k8sapi/common/logs.py
@@ -7,7 +7,7 @@ from conf import conf
 
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     filename=conf.log_file,
     format=('%(asctime)s '
             '[%(levelname)s] '
