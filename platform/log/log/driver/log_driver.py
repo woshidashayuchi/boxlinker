4562d2ab1d1d82950ea4e36c757bd3d2b05d5c31
huay001 <yunshu360@163.com>
2017-03-01 15:44:10 +0800

    更新log服务

--- platform/log/log/driver/log_driver.py
+++ platform/log/log/driver/log_driver.py
@@ -3,6 +3,7 @@
 # Author: YanHua <it-yanh@all-reach.com>
 
 import os
+import time
 import requests
 
 from conf import conf
@@ -17,58 +18,37 @@ class LogDriver(object):
     def __init__(self):
 
         self.url = conf.kibana_log_api
-        self.headers = {'kbn-version': '4.5.4'}
+        self.headers = {"kbn-version": "5.2.1"}
 
-    def pod_log_info(self, label_value, pod_name,
-                     date_time, start_time, end_time):
+    def pod_log_info(self, label_value, date_time,
+                     start_time, end_time):
 
-        body_label = '{"index":["logstash-%s"],"ignore_unavailable":true} \n \
-                {"size":100,"sort":[{"@timestamp": \
-                {"order":"asc","unmapped_type":"boolean"}}], \
-                "query":{"filtered":{"query":{"query_string": \
-                {"analyze_wildcard":false,"query":"*"}}, \
-                "filter":{"bool":{"must":[{"query":{"match": \
-                {"kubernetes.labels.logs":{"query":"%s","type":"phrase"}}}}, \
-                {"range":{"@timestamp":{"gte":%d,"lte":%d, \
-                "format":"epoch_millis"}}}],"must_not":[]}}}}, \
-                "highlight":{"pre_tags":["@kibana-highlighted-field@"], \
-                "post_tags":["@/kibana-highlighted-field@"], \
-                "fields":{"*":{}},"require_field_match":false, \
-                "fragment_size":2147483647},"aggs":{"2": \
-                {"date_histogram":{"field":"@timestamp","interval":"30s", \
-                "time_zone":"Asia/Shanghai","min_doc_count":0, \
-                "extended_bounds":{"min":%d,"max":%d}}}},"fields": \
-                ["*","_source"],"script_fields":{},"fielddata_fields": \
-                ["@timestamp","time"]} \n' \
-               % (str(date_time), label_value, start_time,
-                  end_time, start_time, end_time)
+        now_time = int(round(time.time()*1000))
 
-        body_pod = '{"index":["logstash-%s"],"ignore_unavailable":true} \n \
-                {"size":100,"sort":[{"@timestamp": \
-                {"order":"asc","unmapped_type":"boolean"}}], \
-                "query":{"filtered":{"query":{"query_string": \
-                {"analyze_wildcard":false,"query":"*"}}, \
-                "filter":{"bool":{"must":[ \
-                {"query":{"match":{"kubernetes.pod_name": \
-                {"query":"%s","type":"phrase"}}}}, \
-                {"range":{"@timestamp":{"gte":%d,"lte":%d, \
-                "format":"epoch_millis"}}}],"must_not":[]}}}}, \
-                "highlight":{"pre_tags":["@kibana-highlighted-field@"], \
-                "post_tags":["@/kibana-highlighted-field@"], \
-                "fields":{"*":{}},"require_field_match":false, \
-                "fragment_size":2147483647},"aggs":{"2": \
-                {"date_histogram":{"field":"@timestamp","interval":"30s", \
-                "time_zone":"Asia/Shanghai","min_doc_count":0, \
-                "extended_bounds":{"min":%d,"max":%d}}}},"fields": \
-                ["*","_source"],"script_fields":{},"fielddata_fields": \
-                ["@timestamp","time"]} \n' \
-               % (str(date_time), pod_name, start_time,
-                  end_time, start_time, end_time)
-
-        if pod_name is None:
-            body = body_label
-        else:
-            body = body_pod
+        body = (' {"index": ["logstash-%s"], "ignore_unavailable": true, '
+                ' "preference": %d} \n '
+                ' {"size": 100, '
+                ' "sort": [{"@timestamp": '
+                ' {"order": "asc", "unmapped_type": "boolean"}}], '
+                ' "query": {"bool": {"must": [{"query_string": '
+                ' {"analyze_wildcard": false, "query":"*"}}, '
+                ' {"match": {"kubernetes.labels.logs": '
+                ' {"query": "%s", "type": "phrase"}}}, '
+                ' {"range": {"@timestamp": {"gte": %d, "lte": %d, '
+                ' "format": "epoch_millis"}}}], '
+                ' "must_not": []}}, '
+                ' "highlight": {"pre_tags": ["@kibana-highlighted-field@"], '
+                ' "post_tags": ["@/kibana-highlighted-field@"], '
+                ' "fields": {"*":{}}, "require_field_match": false, '
+                ' "fragment_size": 2147483647}, '
+                ' "_source": {"excludes": []}, '
+                ' "aggs": {"2": {"date_histogram": '
+                ' {"field": "@timestamp", "interval": "30s", '
+                ' "time_zone": "Asia/Shanghai", "min_doc_count": 1}}}, '
+                ' "stored_fields": ["*"], "script_fields": {}, '
+                ' "docvalue_fields": ["@timestamp", "time"]} \n '
+                % (str(date_time), now_time, label_value,
+                   start_time, end_time))
 
         log.debug('body=%s, type=%s' % (body, type(body)))
 
