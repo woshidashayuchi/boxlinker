74ddab780a717ac93b718c47e2f91262aa843552
zhangpei.liu <liuzhangpei@126.com>
2017-03-01 15:48:46 +0800

    ....

--- platform/docs/apidoc/imageAuth/imageAuthdoc/api_project.js
+++ platform/docs/apidoc/imageAuth/imageAuthdoc/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-02-28T01:47:43.548Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
+define({  "name": "",  "version": "0.0.0",  "description": "",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2017-03-01T07:48:01.171Z",    "url": "http://apidocjs.com",    "version": "0.16.1"  }});
